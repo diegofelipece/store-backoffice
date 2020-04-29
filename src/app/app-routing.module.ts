f736562382ca2139f63af699d20ca3e1cbfc9450
Diego Esteves <diegofelipece@gmail.com>
2020-04-29 17:52:48 +0200

    feat: create page componenents

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { ProductViewComponent } from './product-view/product-view.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'employee', component: EmployeeComponent },
+  { path: 'product/add', component: ProductAddComponent },
+  { path: 'product/:id', component: ProductViewComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { ProductViewComponent } from './product-view/product-view.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    EmployeeComponent,
+    ProductViewComponent,
+    ProductAddComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
