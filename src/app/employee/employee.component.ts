0d650f8d9990281f7722c6c432f8b33c24e8bb24
Diego Esteves <diego.esteves@euris.it>
2020-05-04 11:35:07 +0200

    feat: add employee form

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl, Validators, FormGroup, FormBuilder, AbstractControl } from '@angular/forms';
 
 import { EmployeeService } from './employee.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee',
@@ -10,17 +11,46 @@ import { Router } from '@angular/router';
 })
 export class EmployeeComponent implements OnInit {
 
+  employeeForm: FormGroup;
   constructor(
     private employeeService: EmployeeService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
-    // TODO: create form component to get employeeName from user
+    this.employeeForm = this.formBuilder.group({
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(22),
+          Validators.pattern(/^\S/),
+        ]
+      ]
+    });
   }
 
-  setEmployee() {
-    this.employeeService.setEmployee('Diego');
+  setEmployee(employeeName: string) {
+    this.employeeService.setEmployee(employeeName);
     this.router.navigate(['dashboard']);
+  }
+
+  getErrorMessage(fieldName: string) {
+    const errors = this.employeeForm.get(fieldName).errors;
+
+    if (errors && errors?.required){ return 'You should enter a name.'; }
+    if (errors && (errors?.minlength || errors?.maxlength)){ return 'It must have between 3 and 22 chars.'; }
+    if (errors && errors?.pattern){ return 'It can\'t start with empty spaces'; }
+
+    return null;
+  }
+
+  onFormSubmit() {
+    if (this.employeeForm.valid) {
+      const employeeName = this.employeeForm.get('name').value;
+      this.setEmployee(employeeName);
+    }
   }
 }
--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { EmployeeComponent } from './employee.component';
 import { EmployeeService } from './employee.service';
@@ -9,7 +15,12 @@ import { EmployeeService } from './employee.service';
     EmployeeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule
   ],
   providers: [
     EmployeeService
